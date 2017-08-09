c5d80f1f937127eb48653c6f7d366a221428bb33
caprepo <vidavid@LIN73000499.corp.capgemini.com>
2017-08-09 19:34:52 +0530

    greetings Updated.

--- Day2-Demo/src/main/java/org/cap/demo/MainClass.java
+++ Day2-Demo/src/main/java/org/cap/demo/MainClass.java
@@ -4,6 +4,10 @@ public class MainClass {
 
 	public static void greetings(){
 		System.out.println("Hey! Good AfterNoon!");
+		System.out.println("Hey!");
+		System.out.println("Hey! Good Night!");
+		System.out.println("Hey! Welcome!");
+		
 	}
 	
 	public static void main(String[] args) {
